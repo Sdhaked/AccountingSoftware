1852df0e7cf7712014801ee3f047c59b9d4b466d
SaurabhDhakad <saurabh@appentus.in>
2026-08-30 18:33:07 +0530

    Update branding assets and favicon references

--- resources/views/admin/_partials/head/g-links.blade.php
+++ resources/views/admin/_partials/head/g-links.blade.php
@@ -1,6 +1,7 @@
 <!-- Site Icon -->
-<link rel="icon" type="image/png" href="{{ asset('images/account-icon.png') }}" />
-<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+<link rel="icon" href="{{ asset('images/santrains/favicons/cropped-santarin-icon-32x32.png') }}" sizes="32x32" />
+<link rel="icon" href="{{ asset('images/santrains/favicons/cropped-santarin-icon-192x192.png') }}" sizes="192x192" />
+<link rel="apple-touch-icon" href="{{ asset('images/santrains/favicons/cropped-santarin-icon-180x180.png') }}" />
 
 <!-- Font-awesom 6.1.1 Icons CDN -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
